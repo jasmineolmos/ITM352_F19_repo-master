0f2f236e1286d4eb0e70917d71e813fa5b747b2c
Jasminr <jolmos@hawaii.edu>
2019-10-20 16:08:19 -1000

    Update

---
+++ Lab11/Ex1.js
@@ -0,0 +1,17 @@
+/*
+attributes  =  "Jasmine ;19;19.5;18.5" ;
+
+theSeparator = ';'
+parts = attributes.split(theSeparator);
+*/
+parts = ["Jasmine;19;19.5;18.5"] ;
+
+for (i=0; i<parts.length; i++) {
+
+
+console.log(typeof parts [i]);
+
+}
+
+console.log(parts.join(';'));
+
---
+++ Lab11/Ex4.js
@@ -0,0 +1,6 @@
+function isNonNegInt(q) { 
+    console.log('hey');
+
+}
+
+isNonNegInt();
